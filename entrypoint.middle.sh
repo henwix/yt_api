b6a467ec6daadd1aac2c1b60504f9bebc9978478
henwix <492b675xhpd1@gmail.com>
2025-04-24 08:14:51 +0400

    logs for promtail

--- entrypoint.middle.sh
+++ entrypoint.middle.sh
@@ -2,4 +2,6 @@ python manage.py collectstatic --noinput
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 # gunicorn core.project.wsgi:application --timeout 300 --bind 0.0.0.0:8000
-gunicorn core.project.wsgi:application --bind 0.0.0.0:8000
+gunicorn core.project.wsgi:application --access-logfile /var/log/django/access.log --error-logfile /var/log/django/error.log --bind 0.0.0.0:8000 --workers=3
+
+# TODO: проверить логи все: nginx + gunicorn. Далее настраивать мониторинг
